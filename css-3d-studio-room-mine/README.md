b285cb7d786add6fd53965a2e72701092a261760
Barack Flobama <121074306+UnitedFlopsofAmerica@users.noreply.github.com>
2023-02-17 09:50:16 +0100

    Delete css-3d-studio-room-mine directory

--- css-3d-studio-room-mine/README.md
+++
@@ -1,4 +0,0 @@
-# CSS 3D Studio Room (Mine! 😅)
-
-A Pen created on CodePen.io. Original URL: [https://codepen.io/jh3y/pen/XWdQBRx](https://codepen.io/jh3y/pen/XWdQBRx).
-
--- css-3d-studio-room-mine/src/script.js
+++
@@ -1,8 +0,0 @@
-// Purely for toggling audio
-const AUDIO_TOGGLE = document.querySelector('#audio')
-const AUDIO = document.querySelector('.ambience')
-AUDIO.volume = 0.5
-AUDIO_TOGGLE.addEventListener('input', () => {
-  AUDIO.muted = !AUDIO.muted
-  AUDIO.play()
-})
